fbaf9384839f4215fd60a5ed8259d031cf31147c
Oleh Zymenko <oleg.zymenko.13@gmail.com>
2024-08-22 16:28:48 +0300

    Cypher done

--- lib/main.dart
+++ lib/main.dart
@@ -1,107 +1,71 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const FloatingActionButtonExampleApp());
+void main() => runApp(const MyApp());
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     const appTitle = 'Form Styling Demo';
-//     return MaterialApp(
-//       title: appTitle,
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: const Text(appTitle),
-//         ),
-//         body: const MyCustomForm(),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    const appTitle = 'Cypher App';
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(appTitle),
+       //   backgroundColor: Color.fromARGB(255, 223, 248, 248),
+         ),
+        body: MyHomePage(),
+      ),
+    );
+  }
+}
 
-class MyCustomForm extends StatelessWidget {
-  const MyCustomForm({super.key});
-
+class MyHomePage extends StatelessWidget {
+  MyHomePage({super.key});
+  final controller = TextEditingController();
+  final int shift = 5;
+  //int symbol = - ;
+  
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextField(
-            decoration: InputDecoration(
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextFormField(
+            onChanged: (value) => {controller.text = cypherValue(value)},
+            decoration: const InputDecoration(
               border: OutlineInputBorder(),
-              hintText: 'Enter a search term',
+              labelText: 'Введите текст',
             ),
           ),
         ),
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextFormField(
+          child: TextField(
+            controller: controller, 
+            readOnly: true,
             decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'Enter your username',
+              border: OutlineInputBorder(),
+              labelText: 'Закодированный текст', 
             ),
           ),
         ),
       ],
     );
   }
-}
 
+  String cypherValue(String value) {
+    String result = '';
+    for (var i = 0; i < value.length; i++) {
+      String letter = value[i];
+      int code = letter.codeUnitAt(0);
+      int newCode = code + shift;
+      result += String.fromCharCode(newCode);
+    }
 
-class FloatingActionButtonExampleApp extends StatelessWidget {
-  const FloatingActionButtonExampleApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(useMaterial3: true),
-      home: const FloatingActionButtonExample(),
-    );
-  }
-}
-
-class FloatingActionButtonExample extends StatefulWidget {
-  const FloatingActionButtonExample({super.key});
-
-  @override
-  State<FloatingActionButtonExample> createState() =>
-      _FloatingActionButtonExampleState();
-}
-
-class _FloatingActionButtonExampleState
-    extends State<FloatingActionButtonExample> {
-  // The FAB's foregroundColor, backgroundColor, and shape
-  static const List<(Color?, Color? background, ShapeBorder?)> customizations =
-      <(Color?, Color?, ShapeBorder?)>[
-    (null, null, null), // The FAB uses its default for null parameters.
-    (null, Colors.green, null),
-    (Colors.white, Colors.green, null),
-    (Colors.white, Colors.green, CircleBorder()),
-  ];
-  int index = 0; // Selects the customization.
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Cypher Application'),
-      ),
-      body: const Center(child: MyCustomForm()),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            index = (index + 1) % customizations.length;
-          });
-        },
-        foregroundColor: customizations[index].$1,
-        backgroundColor: customizations[index].$2,
-        shape: customizations[index].$3,
-        child: const Icon(Icons.settings),
-      ),
-    );
+    return result;
   }
 }
